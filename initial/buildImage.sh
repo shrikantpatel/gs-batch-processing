a37e0599ffe7934e2864692f4b0eb3580915ecae
shrikantpatel <shrikant_patelin@yahoo.com>
2023-10-19 09:01:08 -0500

    change to deploy this batch service as job on the OCP.

--- initial/buildImage.sh
+++ initial/buildImage.sh
@@ -1,3 +1,4 @@
+docker rmi -f quay.io/shripatel1919/batch-app:latest
 cd ~/projects/gs-batch-processing/initial
 
 rm -rf build/dependency
@@ -11,4 +12,5 @@ cd build/dependency
 jar -xf ../libs/*.jar
 
 cd ~/projects/gs-batch-processing/initial
-docker build -t shrikantpatel/myapp .
+docker build --platform=linux/amd64 -t quay.io/shripatel1919/batch-app:latest .
+docker push quay.io/shripatel1919/batch-app:latest
